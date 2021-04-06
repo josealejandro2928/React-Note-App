9700151a1dcbbcce17f29118b0ff8e1660153094
Jose Alejandro Concepcion Alvarez <jalejandro2928@gmail.com>
2021-04-06 13:10:09 -0500

    Terminado

--- src/App/App.js
+++ src/App/App.js
@@ -1,21 +1,85 @@
 import './App.scss';
 import Note from '../components/Note/Note';
+import React, { useState, useEffect, useRef } from 'react';
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  const containerRef = useRef();
+  const elementRef = containerRef.current;
+
+  useEffect(() => {
+    let tempNotes = JSON.parse(localStorage.getItem('notes') || `[]`);
+    tempNotes.map((x) => (x.isEditing = false));
+    setNotes(tempNotes);
+    // console.log("ppppppppp")
+  }, []);
+
   return (
-    <div className='App'>
+    <div className='App' ref={containerRef}>
       <div className='layout-btn'>
-        <button className='add-btn' id='add'>
+        <button onClick={onAddNote.bind(this)} className='add-btn' id='add'>
           <i className='fas fa-plus'></i>
           Add note
         </button>
       </div>
 
       <div className='container-notes'>
-        <Note></Note>
+        {notes.map((note, index) => {
+          return (
+            <Note
+              key={note.id}
+              note={note}
+              deleteNote={() => onDeleteNote(index)}
+              toggleEditing={(note) => onToggleEditing(note, index)}
+              changeText={(note, text) => onChangeText(note, text, index)}
+            ></Note>
+          );
+        })}
       </div>
     </div>
   );
+
+  function onDeleteNote(index) {
+    notes.splice(index, 1);
+    saveData(notes);
+  }
+
+  function onAddNote() {
+    let newNote = { id: Date.now(), text: '', isEditing: true };
+    notes.push(newNote);
+    saveData(notes);
+    setTimeout(() => {
+      let noteTag = elementRef.querySelector(`#note-${newNote.id} textarea`);
+      noteTag.focus();
+    }, 150);
+  }
+
+  function onChangeText(note,text, index) {
+    note.text = text;
+    notes[index] = note;
+    saveData([...notes]);
+    // console.log(notes);
+  }
+
+  function onToggleEditing(note, index) {
+    note = { ...note, isEditing: !note.isEditing };
+    notes[index] = note;
+    setNotes([...notes]);
+
+    let c = setTimeout(() => {
+      let textarea = elementRef.querySelector(`#note-${note.id} textarea`);
+      if (textarea) {
+        textarea.focus();
+      }
+      clearTimeout(c);
+    }, 200);
+  }
+
+  function saveData(notes = []) {
+    setNotes([...notes]);
+    localStorage.setItem('notes', JSON.stringify(notes));
+  }
 }
 
 export default App;
--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -1,20 +1,55 @@
+/* eslint-disable no-unused-vars */
 import './Note.scss';
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
 
-function Note() {
-  return (
-    <div className='Note'>
-      <div className='note'>
-        <div className='tools'>
-          <button className='edit'>
-            <i className='fas fa-edit'></i>
-          </button>
-          <button className='delete'>
-            <i className='fas fa-trash-alt'></i>
-          </button>
+function Note(props) {
+  let note = props.note;
+  let tagSection;
+
+  if (note.isEditing) {
+    tagSection = (
+      <textarea
+        value={note.text}
+        onChange={(e) => props.changeText(note, e.target.value)}
+      ></textarea>
+    );
+  } else {
+    tagSection = (
+      // <div
+      //   className='text'
+      //   dangerouslySetInnerHTML={{ __html: note.text }}
+      // ></div>
+      <div className='text'>
+        <div className='markdown-body'>
+          <ReactMarkdown>{note.text}</ReactMarkdown>
         </div>
-        <div className='hidden'></div>
-        <textarea></textarea>
+
+        {/* <ReactMarkdown># Hello, *world*!</ReactMarkdown> */}
       </div>
+    );
+  }
+
+  return (
+    <div id={'note-' + note.id} className='Note'>
+      <div className='tools'>
+        <button
+          onClick={() => {
+            props.toggleEditing(note);
+          }}
+          className={note.isEditing ? 'active' : ''}
+        >
+          <i className='fas fa-edit'></i>
+        </button>
+        <button
+          onClick={() => {
+            props.deleteNote(note);
+          }}
+        >
+          <i className='fas fa-trash-alt'></i>
+        </button>
+      </div>
+      {tagSection}
     </div>
   );
 }
